3f96f64f92b4cebc69c6b7d953effa79f2d9409b
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-05-02 21:04:54 +0800

    update general layout to add padding on each children

--- src/layouts/GeneralLayout.tsx
+++ src/layouts/GeneralLayout.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 const GeneralLayout = ({ children }: Props) => {
 	return (
-		<div className='h-screen grid grid-cols-[100px_auto_350px] bg-[#131313]'>
+		<div className='h-screen grid grid-cols-[100px_auto] bg-[#131313]'>
 			<SideNav />
-			<div className='bg-[#202329] p-4 rounded-3xl'>{children}</div>
+			<div className='bg-[#202329]'>{children}</div>
 		</div>
 	);
 };
