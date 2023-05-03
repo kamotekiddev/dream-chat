3be8d4ba086a275a1c3c6fa9e6127f0ce47254d9
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-05-03 19:59:13 +0800

    remove the message avatar of my message

--- src/features/SingleChat/MessageList/MessageListItem.tsx
+++ src/features/SingleChat/MessageList/MessageListItem.tsx
@@ -5,33 +5,35 @@ interface Props {
 const MessageListItem = ({ message }: Props) => {
 	const isMymessage = message?.sender?.id === '1';
 	const messageAligment = isMymessage ? 'self-end' : 'self-start';
-	const messageDirection = isMymessage && 'flex-row-reverse';
+	const messageColor = isMymessage ? 'bg-blue-600' : 'bg-[#2E343D]';
 
 	return (
-		<div className={`flex  gap-5 ${messageDirection} ${messageAligment}`}>
-			<div className='h-14 w-14 bg-gray-500 rounded-xl' />
-			<div className='bg-[#2E343D] rounded-xl p-4 relative bottom-2'>
+		<div className={`flex max-w-lg  gap-5 ${messageAligment}`}>
+			{!isMymessage && (
+				<div className='h-14 flex-shrink-0 w-14 bg-gray-500 rounded-xl' />
+			)}
+			<div className={`${messageColor} h-max rounded-xl px-4 py-2 relative`}>
 				{!isMymessage && (
-					<div>
+					<>
 						<div className='h-4 w-4 bg-[#2E343D] absolute -left-2 bottom-0' />
 						<div
 							className='h-4 w-4 bg-[#202329] absolute -left-4 bottom-0'
 							style={{ borderRadius: '0 0 1rem 0' }}
 						/>
-					</div>
+					</>
 				)}
-				<p className='text-xs'>{message?.text}</p>
+				<p className='text-sm'>{message?.text}</p>
 				{isMymessage && (
-					<div>
+					<>
 						<div
-							className='h-4 w-4 bg-[#2E343D] absolute -right-2 bottom-0'
+							className={`h-4 w-4 absolute -right-2 bottom-0 ${messageColor}`}
 							style={{ borderRadius: '0 0 0 0' }}
 						/>
 						<div
 							className='h-4 w-4 bg-[#202329] absolute -right-4 bottom-0'
 							style={{ borderRadius: '0 0 0 1rem' }}
 						/>
-					</div>
+					</>
 				)}
 			</div>
 		</div>
